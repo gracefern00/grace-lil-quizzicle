31aa6a57ccb96d7cfce63fc1c6526cee121d7ed9
beanbean <gracefernandez00@icloud.com>
2024-02-22 20:45:14 -0700

    CSS final JS add

---
+++ Assets/questionaire.js
@@ -0,0 +1,57 @@
+Let questions = [
+  {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "D. script",
+    options: [
+      "A. javascript",
+      "B. scripting",
+      "C. js",
+      "D. script"
+    ]
+  }
+  {
+    numb: 2,
+    question: "How to write an IF statement in JavaScript?",
+    answer: "B. if (i == 5)",
+    options: [
+      "A. if i == 5 then",
+      "B. if (i == 5)",
+      "C. if i = 5",
+      "D. if i = 5 then"
+    ]
+  }
+  {
+    numb: 3,
+    question: "Which operator is used to assign a value to a variable?",
+    answer: "A. =",
+    options: [
+      "A. =",
+      "B. *",
+      "C. x",
+      "D. -"
+    ]
+  }
+  {
+    numb: 4,
+    question: "How can you add a comment in a JavaScript?",
+    answer: "C. //This is a comment",
+    options: [
+      "A. <!--This is a comment-->",
+      "B. 'This is a comment",
+      "C. //This is a comment",
+      "D. I just made a comment hehe"
+    ]
+  }
+  {
+    numb: 5,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "A. onclick",
+    options: [
+      "A. onclick",
+      "B. onmouseclick",
+      "C. onmouseover",
+      "D. onchange"
+    ]
+  }
+]
--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,21 @@
-//LEFT OFF AT 18:13 in https://www.youtube.com/watch?v=Vp8x8-reqZA&t=4835s
+const startButton = document.querySelector('.start-button');
+const popupInfo = document.querySelector('.popup-info');
+const exitButton = document.querySelector('.exit-button');
+const main = document.querySelector('.main');
+const continueButton = document.querySelector('.continue-button');
+const quizSection = document.querySelector('.quiz-section');
+
+startButton.onclick = () => {
+  popupInfo.classList.add('active');
+}
+
+exitButton.onclick = () => {
+  popupInfo.classList.remove('active');
+  main.classList.remove('active');
+}
+
+continueButton.onclick = () => {
+  quizSection.classList.add('active');
+  popupInfo.classList.remove('active');
+  main.classList.remove('active');
+}
