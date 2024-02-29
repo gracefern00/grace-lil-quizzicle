7b210a323c652a18fc87b71d93a929bbe183ee49
beanbean <gracefernandez00@icloud.com>
2024-02-28 21:25:52 -0700

    JS additions, quiz answer script

--- Assets/script.js
+++ Assets/script.js
@@ -22,20 +22,62 @@ continueButton.onclick = () => {
   quizBox.classList.add('active');
 
   showQuestions(0);
+  questionCounter(1);
 }
 
 let questionCount = 0;
+let questionNumb = 1;
 
 const nextButton = document.querySelector('.next-button');
 
 nextButton.onclick = () => {
-  questionCount++;
-  showQuestions(questionCount);
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestions(questionCount);
+
+    questionNumb++;
+    questionCounter(questionNumb);
+  }
+  else {
+    console.log('Question Answered')
+  }
 }
+
+
+const optionList = document.querySelector(".option-list")
 
 //get Q's and the options from an array
 
 function showQuestions(index) {
   const questionText = document.querySelector('.question-text');
   questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+  <div class="option"><span>${questions[index].options[1]}</span></div>
+  <div class="option"><span>${questions[index].options[2]}</span></div>
+  <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+  optionList.innerHTML = optionTag;
+
+  const option = document.querySelectorAll('.option');
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute('onclick', 'optionSelected(this)');
+  }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+
+  if (userAnswer == correctAnswer) {
+    console.log('correct answer');
+  }
+  else {
+    console.log('wrong answer');
+  }
+}
+
+function questionCounter(index) {
+  const questionTotal = document.querySelector('.question-total');
+  questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
